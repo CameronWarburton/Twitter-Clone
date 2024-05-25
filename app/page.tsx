ff7b63157c48266c39c123571335af026b1fba27
Cameron <cameron.cw@outlook.com>
2024-05-25 23:24:44 +0100

    Started on Navigation Bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,63 @@
+import Link from "next/link";
+import { BiHomeCircle, BiUser } from "react-icons/bi";
+import { BsBell, BsBookmark, BsTwitter } from "react-icons/bs";
+import { HiOutlineHashtag } from "react-icons/hi";
+import { HiEnvelope } from "react-icons/hi2";
+
+const NAVIGATION_ITEMS = [
+  {
+    title: "Home",
+    icon: BiHomeCircle,
+  },
+  {
+    title: "Explore",
+    icon: HiOutlineHashtag,
+  },
+  {
+    title: "Notifications",
+    icon: BsBell,
+  },
+  {
+    title: "Explore",
+    icon: HiEnvelope,
+  },
+  {
+    title: "Booksmarks",
+    icon: BsBookmark,
+  },
+  {
+    title: "Profile",
+    icon: BiUser,
+  },
+];
+
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="w-full h-full flex justify-center items-center relative bg-black">
+      <div className="max-w-screen-lg w-full h-full flex relative">
+        {/* left sidebar for navigation */}
+        <section className="fixed w-72 flex-col h-screen space-y-4 my-4">
+          <Link href={"/"} className="p-2 text-2xl">
+            <BsTwitter />
+          </Link>
+          {NAVIGATION_ITEMS.map((item) => (
+            <Link
+              className="hover:bg-white/10 text-2xl transition duration-200 flex items-center justify-start w-fit space-x-4 rounded-3xl p-2"
+              href={`/${item.title.toLocaleLowerCase()}`}
+              key={item.title}
+            >
+              <div>
+                <item.icon />
+              </div>
+              <div>{item.title}</div>
+            </Link>
+          ))}
+        </section>
+        {/* <main>home timeline</main> */}
+        {/* <section>right section</section> */}
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
