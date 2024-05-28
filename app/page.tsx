bf57302ed84ce8ac3c12ee81ae1e419b7990ec67
Cameron <cameron.cw@outlook.com>
2024-05-28 12:42:50 +0100

    Moved Left Side Bar to it's file in components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import Link from "next/link";
 import { BiHomeCircle, BiUser } from "react-icons/bi";
-import { BsBell, BsBookmark, BsTwitter, BsEnvelope, BsThreeDots } from "react-icons/bs";
+import {
+  BsBell,
+  BsBookmark,
+  BsTwitter,
+  BsEnvelope,
+  BsThreeDots,
+} from "react-icons/bs";
 import { HiOutlineHashtag } from "react-icons/hi";
+import LeftSidebar from "./components/LeftSidebar";
 
 const NAVIGATION_ITEMS = [
   {
@@ -39,38 +46,7 @@ export const Home = () => {
     <div className="w-full h-full flex justify-center items-center relative bg-black">
       <div className="max-w-screen-xl w-full h-full flex relative">
         {/* left sidebar for navigation */}
-        <section className="fixed w-[275px] flex flex-col items-stretch h-screen">
-          <div className="flex flex-col items-stretch h-full space-y-4 mt-4">
-            {NAVIGATION_ITEMS.map((item) => (
-              <Link
-                className="hover:bg-white/10 text-2xl transition duration-200 flex items-center justify-start w-fit space-x-4 rounded-3xl py-2 px-6"
-                href={`/${item.title.toLocaleLowerCase()}`}
-                key={item.title}
-              >
-                <div>
-                  <item.icon />
-                </div>
-                {item.title !== "Twitter" && <div>{item.title}</div>}
-              </Link>
-            ))}
-            <button className="rounded-full m-4 bg-primary p-4 text-2xl text-center hover:bg-opacity-70 transition duration-200">
-              Tweet
-            </button>
-          </div>
-          <button className="rounded-full flex items-center space-x-2 m-4 bg-transparent p-4 text-center hover:bg-opacity-70 transition duration-200">
-              
-              <div className="rounded-full bg-slate-400 w-8 h-8">
-
-              </div>
-              <div className="text-left text-xs">
-                <div className="font-semibold">Cameron</div>
-                <div className="">@CameronW</div>
-              </div>
-              <div>
-                <BsThreeDots/>
-              </div>
-            </button>
-        </section>
+       <LeftSidebar/>
         {/* <main>home timeline</main> */}
         {/* <section>right section</section> */}
       </div>
