8977c033b96fc607e7e87ac6df9cb93c3506edc0
Cameron <cameron.cw@outlook.com>
2024-05-29 22:22:10 +0100

    Added Search Box

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { BsSearch } from "react-icons/bs";
 import LeftSidebar from "./components/LeftSidebar";
 import MainComponent from "./components/MainComponent";
 
@@ -5,10 +6,28 @@ const Home = () => {
   return (
     <div className="w-full h-full flex justify-center items-center relative bg-black text-white">
       <div className="max-w-screen-xl w-full h-full flex relative">
-        {/* left sidebar for navigation */}
         <LeftSidebar />
         <MainComponent />
-        {/* <section>right section</section> */}
+        <section className="fixed flex flex-col space-y-4 right-0">
+          <div>
+            <div className="relative w-fill h-full ">
+              <label
+                htmlFor="searchBox"
+                className="absolute top-0 left-0 h-full flex items-center justify-center"
+              >
+                <BsSearch className="w-5 h-5 text-grey-500" />
+              </label>
+              <input
+                id="searchBox"
+                type="text"
+                placeholder="Search Twitter"
+                className="outline-none bg-transparent border-none w-full h-full rounded-xl py-4 px-8"
+              />
+            </div>
+          </div>
+          <div></div>
+          <div></div>
+        </section>
       </div>
     </div>
   );
