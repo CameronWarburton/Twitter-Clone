858b5ebb00dbb5bffa15d719738d10a5e4dba400
Cameron <cameron.cw@outlook.com>
2024-06-07 13:00:53 +0100

    Tweakes to Left and Right Sidebar

--- components/MainComponent.tsx
+++ components/MainComponent.tsx
@@ -3,30 +3,17 @@
 import { AiOutlineHeart, AiOutlineRetweet } from "react-icons/ai";
 import { BsChat, BsDot, BsThreeDots } from "react-icons/bs";
 import { IoShareOutline, IoStatsChart } from "react-icons/io5";
+import ComposeTweet from "./server-component/compose-tweet";
 
 const MainComponent = () => {
   return (
-    <main className="flex w-[50%] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
+    <main className="flex w-full xl:w-[50%] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
       <h1 className="text-xl font-bold p-6 backdrop-blur bg-black/10 sticky top-0">
         Home
       </h1>
       <div className="border-t-[0.5px] px-4 border-b-[0.5px] flex items-stretch py-6 space-x-2 border-gray-600 relative">
         <div className="w-11 h-11 bg-slate-400 rounded-full flex-none"></div>
-        <div className="flex flex-col w-full h-full">
-          <input
-            type="text"
-            className="w-full h-full text-2xl placeholder:text-gray-600 bg-transparent border-b-[0.5px] border-gray-600 p-4 outline-none border-none"
-            placeholder="What's happening?"
-          />
-          <div className="w-full justify-between items-center flex">
-            <div></div>
-            <div className="w-full max-w-[100px]">
-              <button className="rounded-full bg-twitterColor px-4 py-2 w-full text-lg text-center hover:bg-opacity-70 transition duration-200 font-bold">
-                Tweet
-              </button>
-            </div>
-          </div>
-        </div>
+        <ComposeTweet />
       </div>
       <div className="flex flex-col">
         {Array.from({ length: 5 }).map((_, i) => (
