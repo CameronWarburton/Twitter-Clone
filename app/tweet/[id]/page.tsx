fb9f2438a4ab0c6c5aca6d4bc54e95016ab2e7e0
Cameron <cameron.cw@outlook.com>
2024-06-30 17:18:45 +0100

    Finished Replies

--- app/tweet/[id]/page.tsx
+++ app/tweet/[id]/page.tsx
@@ -1,11 +1,16 @@
 import Tweet from "@/components/client-components/tweet";
 import { db } from "@/lib/db";
-import { likes, profiles, tweets } from "@/lib/db/schema";
+import { likes, profiles, replies, tweets } from "@/lib/db/schema";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import React from "react";
 import { cookies } from "next/headers";
 import { and, eq, exists } from "drizzle-orm";
 import { getTweets } from "@/lib/supabase/queries";
+import { BsDot, BsThreeDots } from "react-icons/bs";
+import { AiOutlineRetweet } from "react-icons/ai";
+import ReplyDialog from "@/components/client-components/reply-dialog";
+import LikeButton from "@/components/client-components/like-button";
+import { IoShareOutline, IoStatsChart } from "react-icons/io5";
 
 const TweetPage = async ({ params }: { params: { id: string } }) => {
   const supabaseClient = createServerComponentClient({
@@ -17,10 +22,11 @@ const TweetPage = async ({ params }: { params: { id: string } }) => {
 
   const tweet = await getTweets(userData.user?.id, params.id);
 
-  const replies = await db.query.replies.findMany({
+  const repliesRes = await db.query.replies.findMany({
     with: {
       profile: true,
     },
+    where: eq(replies.tweetId, params.id),
   });
 
   console.log(replies);
@@ -40,6 +46,35 @@ const TweetPage = async ({ params }: { params: { id: string } }) => {
       ) : (
         <div>no tweet found</div>
       )}
+      {repliesRes.map((reply) => (
+        <div
+          key={reply.id}
+          className="border-b-[0.5px] border-gray-600 p-2 flex space-x-4 w-full"
+        >
+          <div>
+            <div className="w-10 h-10 bg-slate-200 rounded-full" />
+          </div>
+          <div className="flex flex-col w-full">
+            <div className="flex items-center w-full justify-between">
+              <div className="flex items-center space-x-1 w-full">
+                <div className="font-bold">{reply.profile.fullName ?? ""}</div>
+                <div className="text-gray-500">@{reply.profile.username}</div>
+                <div className="text-gray-500">
+                  <BsDot />
+                </div>
+              </div>
+              <div>
+                <BsThreeDots />
+              </div>
+            </div>
+            <div
+              className="text-white text-base w-full transition-all"
+            >
+              {reply.text}
+            </div>
+          </div>
+        </div>
+      ))}
     </main>
   );
 };
